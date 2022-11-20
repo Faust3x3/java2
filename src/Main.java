50d24a7cfd901d98216a311d762ef3c65350232c
Arrow234 <forgit@faust3x3.com>
2022-11-20 19:53:49 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 9875;
+        int miles = 20;
+
+        int bonus = cost/miles;
+        System.out.println("Количество начисленных бонусных миль: " + bonus);
+    }
+
+}
